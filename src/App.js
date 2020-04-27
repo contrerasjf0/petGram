489a790822cd84e12db9d8ebb14f011ab7475576
Juan Francisco <contrerasjf.0@hotmail.com>
2020-04-27 04:23:56 -0500

    feature(ListOfCategories): Implement the ListOfCategories component
    * The ListOfCategories component  was implemented.
    * The GlobalStyles component was created to handle the global style with style-components.
    * The code was formated with standard rules.
    * The Prettier rc was created.
    
    Resolve: C-08

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Category } from './components/Category'; 
+import React from 'react'
 
+import { ListOfCategories } from './components/ListOfCategories'
 
-export const App = (params) => {
-    return (
-        <Category />
-    );
-};
+import { GlobalStyle } from './GlobalStyles'
+
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
+)
