84ce5a99cce31606d655217c95874e7483168f8f
Juan Francisco <contrerasjf.0@hotmail.com>
2020-05-01 18:50:21 -0500

    feature(PhotoCard): Implement the like feature with localstorage
    Resolve: C-18

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useRef, useState} from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const key = `like-${id}`
   const element = useRef(null)
   const [show, setShow] = useState(false)
 
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (e) {
+      return false
+    }
+  })
+    
   useEffect(function () {
     Promise.resolve(
       typeof window.IntersectionObserver !== 'undefined'
@@ -25,6 +35,18 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     })
   }, [element])
 
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
   return (
     <Article ref={element}>
       {
@@ -37,8 +59,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes!
+            <Button onClick={() => setLocalStorage(!liked)}>
+              <Icon size='32px' /> {likes} likes!
             </Button>
           </>
         )
