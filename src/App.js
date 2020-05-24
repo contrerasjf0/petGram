f3d1301a811d4ce8268c51d05e61152f45c49082
Juan Francisco <contrerasjf.0@hotmail.com>
2020-05-24 10:05:23 -0500

    feature(PhotoCard): Implement a container component to featch the data of one photo
    * The PhotoCardWithQuery container component was created to featch the photo data and to render
    	the PhotoCard component, The render props pattern was implemented  with the Query component.
    * The App component was updated to rende a view depending on the URL params.
    * The path of 'a' element was changed in the PhotoCard component.
    
    Resolve: C-22

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import { ListOfCategories } from './components/ListOfCategories'
 
 import { GlobalStyle } from './styles-components/GlobalStyles'
 import { ListOfPhotoCards } from './conteiners/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './conteiners/PhotoCardWithQuery'
+
 import { Logo } from './components/Logo'
 
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </div>
-)
+export const App = () => {
+
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const datailIdParam = urlParams.get('detail')
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        (datailIdParam)
+          ? (<PhotoCardWithQuery id={datailIdParam} />)
+          : (
+            <>
+              <ListOfCategories />
+              <ListOfPhotoCards categoryId={1} />
+            </>)
+      }
+    </div>
+  )
+}
