78263dff80da003409548c80e150640fce669223
Juan Francisco <contrerasjf.0@hotmail.com>
2020-06-13 22:54:06 -0500

    :recycle: refactor(Routes): Restructuring of private routes management
    * The App file was updated to restructur the private routes and handle the redirects.
    * The NotFound pages was created.
    
    Resolve: C-41

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Router } from '@reach/router'
+import React, { useContext } from 'react'
+import { Router, Redirect } from '@reach/router'
 
 import { GlobalStyle } from './styles-components/GlobalStyles'
 import { Logo } from './components/Logo'
@@ -10,36 +10,31 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
-import Context from './Context'
+import { Context } from './Context'
 
 export const App = () => {
+
+  const { isAuth } = useContext(Context)
 
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>)
-              : (
-                <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>)
-        }
-      </Context.Consumer>
+
       <NavBar />
     </div>
   )
